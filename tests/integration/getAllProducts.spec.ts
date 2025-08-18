d3edf19baa1840f6783eaba9a2b3575abde27877
ROBERTA MEYRELLES FRANÇA <roberta.franca@uvvnet.com.br>
2025-08-18 20:36:47 -0300

    test: implementação do teste unitário e de integração da rota

---
+++ tests/integration/getAllProducts.spec.ts
@@ -0,0 +1,39 @@
+import request from "supertest";
+import app from "../../src/index";
+import mongoose from "mongoose";
+import { productModel } from "../../src/infra/database/models/productModel";
+import { Types } from "mongoose";
+
+describe("GET /products", () => {
+
+  beforeAll(async () => {
+    await mongoose.connect(process.env.MONGO_URL_TEST!);
+
+  });
+
+  afterAll(async () => {
+    await mongoose.connection.close();
+  });
+
+  beforeEach(async () => {
+    await productModel.deleteMany({});
+    await productModel.create([
+      { name: "Monitor", price: 899 },
+      { name: "Teclado", price: 199 },
+    ]);
+  });
+
+  it("should return all the products", async () => {
+
+    const response = await request(app).get(`/products`);
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveLength(2);
+  });
+
+});
+
+
+
+
+
---
+++ tests/unit/products.spec.ts
@@ -0,0 +1,44 @@
+import { Product } from "../../src/models/Product";
+import { ProductService } from "../../src/services/products";
+import { ProductRepository } from "../../src/repositories/ProductRepository";
+
+describe("ProductService", () => {
+  let repositoryMock: jest.Mocked<ProductRepository>;
+  let service: ProductService;
+
+  beforeEach(() => {
+    //Cria um mock com todos os métodos do repositório
+    repositoryMock = {
+      findAll: jest.fn(),
+    };
+
+    service = new ProductService(repositoryMock);
+  });
+
+  it("deve retornar todos os produtos", async () => {
+    const fakeProducts: Product[] = [
+      {
+        id: "1",
+        name: "Teclado",
+        price: 300
+      },
+      {
+        id: "2",
+        name: "Monitor",
+        price: 3000
+      },
+    ];
+
+    repositoryMock.findAll.mockResolvedValue(fakeProducts);
+
+    const correspondingProducts = await service.getAll();
+
+    expect(correspondingProducts).toEqual(fakeProducts);
+
+  });
+
+
+
+
+
+})
